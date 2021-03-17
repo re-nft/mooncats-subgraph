c35b58c3bac5ba217d4bdc32e809bcc79d8f47d0
nazariyv <nazariy@inbox.ru>
2021-03-17 09:10:53 +0000

    clean subgraph

--- mappings/core.ts
+++ mappings/core.ts
@@ -10,100 +10,84 @@ import {
   AdoptionRequestCancelled,
 } from "../generated/MoonCatRescue/MoonCatRescue";
 import {
-  fetchCat,
-  fetchMoonRescuer,
-  createAdoptionOffered,
-  createAdoptionRequested,
+  createCat,
+  getCat,
+  createOwner,
+  getOwner,
+  fetchOwner,
+  createRequestPrice,
+  getRequestPrice,
+  createOfferPrice,
+  getOfferPrice,
+  createProvenance,
+  getProvenance,
+  fetchProvenance
 } from "./helpers";
 
 let wrapperContract = "0x7c40c393dc0f283f318791d746d894ddd3693572";
 
-export function handleCatAdopted(event: CatAdopted): void {
-  let params = event.params;
-  let catId = params.catId;
-  let cat = fetchCat(catId);
-  let from = fetchMoonRescuer(params.from);
-  let to = fetchMoonRescuer(params.to);
-  cat.owner = params.to.toHexString();
-  cat.inWallet = true;
-  to.save();
-  cat.save();
-  from.save();
-}
+// catNamed only once
+// catRescued once - creation time
 
+// adoptionRequests and adoptionOffers are created and cancelled
+// catAdopted with a matching request/offer means filled request/order
+
+// catAdopted - every time that transferCat is called. owner changes here to to
+
+// this is the only place where the cat is created
 export function handleCatRescued(event: CatRescued): void {
-  let params = event.params;
-  let catId = params.catId;
-  let cat = fetchCat(catId);
-  let to = fetchMoonRescuer(params.to);
-  cat.owner = params.to.toHexString();
-  cat.inWallet = false;
-  to.save();
+  let cat = createCat(event.params.catId.toHex(), event.params.to.toHex(), event.block.timestamp);
   cat.save();
 }
 
-export function handleCatNamed(event: CatNamed): void {
-  let catNamedParams = event.params;
-  let catId = catNamedParams.catId;
-  let cat = fetchCat(catId);
-  cat.name = catNamedParams.catName.toHexString();
-  cat.save();
+// called on every transferCat call
+// 1. acceptAdoptionOffer   - someone sold their cat on their  terms
+// 2. acceptAdoptionRequest - someone sold their cat on else's terms
+// 3. giveCat - feeling generous
+export function handleCatAdopted(event: CatAdopted): void {
+
 }
 
-// * I own a cat, and I am offering toAddress to buy it from me for price
-// you as the owner, are offering someone, to buy the cat from you
+// * I have a cat, you [0x123..333] (or any1 [0x0]) can buy it from me for X wei
+// * push to the list of offer prices, set active = true
 export function handleAdoptionOffered(event: AdoptionOffered): void {
-  let params = event.params;
-  let catId = params.catId;
-  let cat = fetchCat(catId);
-  let adoptionOffer = createAdoptionOffered(
-    cat.id,
-    params.price,
-    params.toAddress,
-    event.block.timestamp
-  );
-  cat.adoptionOffered = adoptionOffer.id;
-  if (params.toAddress.toHexString() == wrapperContract) {
-    cat.wasWrapped = true;
-  }
-  adoptionOffer.save();
-  cat.save();
+  let offerPriceId = getOfferPriceId(event);
+  let provenanceId = event.params.catId.toHex();
+  let offerPrice = createOfferPrice(offerPriceId, provenanceId, event);
+  offerPrice.save();
 }
 
+// * Do not remove the OfferPrice, but set the active field to false
 export function handleAdoptionOfferCancelled(
   event: AdoptionOfferCancelled
 ): void {
-  let params = event.params;
-  let catId = params.catId;
-  let cat = fetchCat(catId);
-  store.remove('AdoptionOffered', cat.id);
-  cat.adoptionOffered = null;
-  cat.save();
+  // let offerPriceId = getOfferPriceId()
 }
 
 // * I like a cat, and I am offering a cat owner, to buy it from (me) for price
 export function handleAdoptionRequested(event: AdoptionRequested): void {
-  let params = event.params;
-  let catId = params.catId;
-  let cat = fetchCat(catId);
-  let adoptionRequest = createAdoptionRequested(
-    cat.id,
-    params.price,
-    params.from,
-    event.block.timestamp
-  );
-  cat.adoptionRequested = adoptionRequest.id;
-  adoptionRequest.save();
-  cat.save();
 }
 
 export function handleAdoptionRequestCancelled(
   event: AdoptionRequestCancelled
 ): void {
-  let params = event.params;
-  let catId = params.catId;
+}
+
+
+
+
+
+export function handleCatNamed(event: CatNamed): void {
+  let catId = event.params.catId;
   let cat = fetchCat(catId);
-  store.remove('AdoptionRequested', cat.id)
-  cat.adoptionRequested = null;
+  cat.name = event.params.catName.toString();
   cat.save();
+}
+
+export function getOfferPriceId(event: AdoptionOffered): string {
+  return event.params.catId.toHex() + "::" + event.transactionLogIndex.toHex();
+}
+
+export function getRequestPriceId(event: AdoptionRequested): string {
+  return event.params.catId.toHex() + "::" + event.transactionLogIndex.toHex();
 }
--- mappings/helpers.ts
+++ mappings/helpers.ts
@@ -1,69 +1,100 @@
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
+import {
+  CatRescued,
+  AdoptionOffered,
+  AdoptionRequested,
+} from "../generated/MoonCatRescue/MoonCatRescue";
+
 import {
   Cat,
-  MoonRescuer,
-  AdoptionRequested,
-  AdoptionOffered,
+  Owner,
+  RequestPrice,
+  OfferPrice,
+  Provenance,
 } from "../generated/schema";
 
-let ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const createCat = (catId: string, ownerId: string, event: CatRescued): Cat => {
+  let cat = new Cat(catId);
+  let owner = fetchOwner(ownerId);
+  owner.save();
+  cat.owner = owner.id;
+  cat.rescueTimestamp = event.block.timestamp;
+  cat.isWrapped = false;
+  return <Cat>cat;
+}
 
-export const fetchCat = (id: Bytes): Cat => {
-  let _id = id.toHexString();
-  let cat = Cat.load(_id);
-  if (cat === null) {
-    cat = new Cat(_id);
-    cat.name = null;
-    cat.owner = ZERO_ADDRESS;
-    cat.inWallet = false;
-    cat.adoptionRequested = null;
-    cat.adoptionOffered = null;
-    cat.wasWrapped = false;
-  }
+export const getCat = (catId: string): Cat => {
+  let cat = Cat.load(catId);
   return <Cat>cat;
 };
 
-export const fetchMoonRescuer = (address: Bytes): MoonRescuer => {
-  let _address = address.toHexString();
-  let moonRescuer = MoonRescuer.load(_address);
-  if (moonRescuer === null) {
-    moonRescuer = new MoonRescuer(_address);
+export const createOwner = (ownerId: string): Owner => {
+  let owner = new Owner(ownerId);
+  return <Owner>owner;
+}
+
+export const getOwner = (ownerId: string): Owner => {
+  let owner = Owner.load(ownerId);
+  return <Owner>owner;
+};
+
+export const fetchOwner = (ownerId: string): Owner => {
+  let owner = getOwner(ownerId);
+  if (owner == null) {
+    owner = createOwner(ownerId);
   }
-  return <MoonRescuer>moonRescuer;
-};
+  return <Owner>owner;
+}
 
-export const createAdoptionRequested = (
-  id: string,
-  price: BigInt,
-  from: Address,
-  createdAt: BigInt
-): AdoptionRequested => {
-  let adoptionRequest = new AdoptionRequested(id);
-  adoptionRequest.price = price;
-  adoptionRequest.from = from;
-  adoptionRequest.timestamp = createdAt;
-  return <AdoptionRequested>adoptionRequest;
-};
+export const createRequestPrice = (requestId: string, provenanceId: string, event: AdoptionRequested): RequestPrice => {
+  let requestPrice = new RequestPrice(requestId);
+  let provenance = fetchProvenance(provenanceId);
+  provenance.save();
+  requestPrice.provenance = provenance.id;
+  requestPrice.price = event.params.price;
+  requestPrice.from = event.params.from;
+  requestPrice.timestamp = event.block.timestamp;
+  requestPrice.filled = false;
+  requestPrice.active = true;
+  return <RequestPrice>requestPrice;
+}
 
-export const fetchAdoptionRequest = (id: string): AdoptionRequested => {
-  let adoptionRequested = AdoptionRequested.load(id);
-  return <AdoptionRequested>adoptionRequested;
-};
+export const getRequestPrice = (requestId: string): RequestPrice => {
+  let requestPrice = RequestPrice.load(requestId);
+  return <RequestPrice>requestPrice;
+}
 
-export const createAdoptionOffered = (
-  id: string,
-  price: BigInt,
-  toAddress: Address,
-  createdAt: BigInt
-): AdoptionOffered => {
-  let adoptionOffer = new AdoptionOffered(id);
-  adoptionOffer.price = price;
-  adoptionOffer.toAddress = toAddress;
-  adoptionOffer.timestamp = createdAt;
-  return <AdoptionOffered>adoptionOffer;
-};
+export const createOfferPrice = (offerId: string, provenanceId: string, event: AdoptionOffered): OfferPrice => {
+  let offerPrice = new OfferPrice(offerId);
+  let provenance = fetchProvenance(provenanceId);
+  provenance.save();
+  offerPrice.provenance = provenance.id;
+  offerPrice.price = event.params.price;
+  offerPrice.to = event.params.toAddress;
+  offerPrice.timestamp = event.block.timestamp;
+  offerPrice.filled = false;
+  offerPrice.active = true;
+  return <OfferPrice>offerPrice;
+}
 
-export const fetchAdoptionOffer = (id: string): AdoptionOffered => {
-  let adoptionOffered = AdoptionOffered.load(id);
-  return <AdoptionOffered>adoptionOffered;
-};
+export const getOfferPrice = (requestId: string): RequestPrice => {
+  let requestPrice = RequestPrice.load(requestId);
+  return <RequestPrice>requestPrice;
+}
+
+export const createProvenance = (provenanceId: string): Provenance => {
+  let provenance = new Provenance(provenanceId);
+  return <Provenance>provenance;
+}
+
+export const getProvenance = (provenanceId: string): Provenance => {
+  let provenance = Provenance.load(provenanceId);
+  return <Provenance>provenance;
+}
+
+export const fetchProvenance = (provenanceId: string): Provenance => {
+  let provenance = getProvenance(provenanceId);
+  if (provenance == null) {
+    provenance = createProvenance(provenanceId);
+  }
+  return <Provenance>provenance;
+}
